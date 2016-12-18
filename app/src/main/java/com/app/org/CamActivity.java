09809474599743d3f52988a6e04fdd481518945c
Hippomormor <cgenter@oncable.dk>
2016-12-18 23:42:37 +0100

    Added control to cam_view

--- app/src/main/java/com/app/org/CamActivity.java
+++ app/src/main/java/com/app/org/CamActivity.java
@@ -2,10 +2,22 @@ package com.app.org;
 
 import android.os.Bundle;
 
+import com.google.common.collect.Lists;
+
 import org.ros.address.InetAddressFactory;
 import org.ros.android.BitmapFromCompressedImage;
 import org.ros.android.RosActivity;
 import org.ros.android.view.RosImageView;
+import org.ros.android.view.VirtualJoystickView;
+import org.ros.android.view.visualization.VisualizationView;
+import org.ros.android.view.visualization.layer.CameraControlLayer;
+import org.ros.android.view.visualization.layer.LaserScanLayer;
+import org.ros.android.view.visualization.layer.Layer;
+import org.ros.android.view.visualization.layer.OccupancyGridLayer;
+import org.ros.android.view.visualization.layer.PathLayer;
+import org.ros.android.view.visualization.layer.PosePublisherLayer;
+import org.ros.android.view.visualization.layer.PoseSubscriberLayer;
+import org.ros.android.view.visualization.layer.RobotLayer;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 
@@ -14,6 +26,8 @@ import sensor_msgs.CompressedImage;
 public class CamActivity extends RosActivity {
 
     private RosImageView<CompressedImage> image;
+    private VirtualJoystickView virtualJoystickView;
+    private VisualizationView visualizationView;
 
     public CamActivity() {
         super("Image Transport", "Image Transport");
@@ -28,6 +42,8 @@ public class CamActivity extends RosActivity {
         image.setTopicName("/camera/rgb/image_color/compressed");
         image.setMessageType(sensor_msgs.CompressedImage._TYPE);
         image.setMessageToBitmapCallable(new BitmapFromCompressedImage());
+
+        virtualJoystickView = (VirtualJoystickView) findViewById(R.id.virtual_joystick);
     }
 
     @Override
@@ -36,5 +52,6 @@ public class CamActivity extends RosActivity {
                 NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(),
                         getMasterUri());
         nodeMainExecutor.execute(image, nodeConfiguration.setNodeName("android/video_view"));
+        nodeMainExecutor.execute(virtualJoystickView, nodeConfiguration.setNodeName("virtual_joystick"));
     }
 }
